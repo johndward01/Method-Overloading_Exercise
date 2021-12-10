db9fbfcc1bb6a25bcc30b503990d7aad28d17052
John Ward <59837086+johndward01@users.noreply.github.com>
2021-12-10 16:23:08 -0600

    Update Program.cs

--- Method-Overloading_Exercise-01-Answers/Program.cs
+++ Method-Overloading_Exercise-01-Answers/Program.cs
@@ -8,36 +8,25 @@ namespace Method_Overloading_Exercise_01_Answers
         static void Main(string[] args)
         {
             //// TODO: Call each of the overloads here (pass in the values directly)
-            //PrintItems(10); // int
-            //PrintItems(1, 2, 3, 4, 5); // params int[]
-            //PrintItems(.222); // double
-            //PrintItems(.1, .2, .3); // params double[]
-            //PrintItems(72.278264262678M); // decimal
-            //PrintItems(72.2788M, 120.34543M, 12.111111384900099M); // params decimal[]
-            //PrintItems(true);// bool
-            //PrintItems(true, false, true, false, false, true); // params bool[]
-            //PrintItems("A string argument"); // string
-            //PrintItems("Multiple", "string", "arguments"); // params string[]
-            //PrintItems('A'); // char 
-            //PrintItems('a', '%', 'Y', 'j', 'E', '&'); // char[]
-            //PrintItems(.1f); // float
-            //PrintItems(.1f, .2f, .3f, 4f); // params float[]
-            //PrintItems(121242L); // long
-            //PrintItems(1L, 2L, 3L, 4L, 38478748349283982L); // params long[]
+            //PrintItems(1, 2, 3, 4, 5); 
+            //PrintItems(.1, .2, .3); 
+            //PrintItems(72.2788M, 120.34543M, 12.111111384900099M);         
+            //PrintItems(true, false, true, false, false, true); 
+            //PrintItems("Multiple", "string", "arguments"); 
+            //PrintItems('a', '%', 'Y', 'j', 'E', '&'); 
+            //PrintItems(.1f, .2f, .3f, 4f); 
+            //PrintItems(1L, 2L, 3L, 4L, 38478748349283982L); 
 
             // Bonus
             // TODO: Call the PrintItems overload and pass in a System.Object as an argument
             PrintItems(new System.Object()); // object
 
             // Note: for the params object[] overloads you might need to create 3 objects then pass those variables into the method as the arguments
-
             // Example 1:
             var str = "Hello World!";
             var num1 = 27;
             var symbol1 = '$';            
             PrintItems(str, num1, symbol1);
-
-
 
             // Example 2:
             var arr = new int[] { 1, 2, 3 };
@@ -70,13 +59,6 @@ namespace Method_Overloading_Exercise_01_Answers
 
 
 
-        // TODO: Create a PrintItems() double overload
-        public static void PrintItems(double item)
-        {
-            Console.WriteLine(item);
-        }
-
-
 
         // TODO: Create a PrintItems() double overload that prints out ANY amount of doubles passed in to the function as arguments
         public static void PrintItems(params double[] items)
@@ -87,13 +69,6 @@ namespace Method_Overloading_Exercise_01_Answers
             }
         }
 
-
-
-        // TODO: Create a PrintItems() decimal overload
-        public static void PrintItems(decimal item)
-        {
-            Console.WriteLine(item);
-        }
 
 
 
@@ -108,13 +83,6 @@ namespace Method_Overloading_Exercise_01_Answers
 
 
 
-        // TODO: Create a PrintItems() bool overload
-        public static void PrintItems(bool item)
-        {
-            Console.WriteLine(item);
-        }
-
-
 
         // TODO: Create a PrintItems() bool overload that prints out ANY amount of bools passed in to the function as arguments
         public static void PrintItems(params bool[] items)
@@ -125,13 +93,6 @@ namespace Method_Overloading_Exercise_01_Answers
             }
         }
 
-
-
-        // TODO: Create a PrintItems() string overload
-        public static void PrintItems(string item)
-        {
-            Console.WriteLine(item);
-        }
 
 
 
@@ -146,13 +107,6 @@ namespace Method_Overloading_Exercise_01_Answers
 
 
 
-        // TODO: Create a PrintItems() char overload
-        public static void PrintItems(char item)
-        {
-            Console.WriteLine(item);
-        }
-
-
 
         // TODO: Create a PrintItems() char overload that prints out ANY amount of chars passed in to the function as arguments
         public static void PrintItems(params char[] items)
@@ -165,13 +119,6 @@ namespace Method_Overloading_Exercise_01_Answers
 
 
 
-        // TODO: Create a PrintItems() float overload
-        public static void PrintItems(float item)
-        {
-            Console.WriteLine(item);
-        }
-
-
 
         // TODO: Create a PrintItems() float overload that prints out ANY amount of floats passed in to the function as arguments
         public static void PrintItems(params float[] items)
@@ -180,14 +127,6 @@ namespace Method_Overloading_Exercise_01_Answers
             {
                 Console.WriteLine(item);
             }
-        }
-
-
-
-        // TODO: Create a PrintItems() long overload
-        public static void PrintItems(long item)
-        {
-            Console.WriteLine(item);
         }
 
 
@@ -203,19 +142,7 @@ namespace Method_Overloading_Exercise_01_Answers
 
 
 
-
-
         // BONUS
-
-        // TODO: Create a PrintItems() object overload
-        public static void PrintItems(object item)
-        {
-            Console.WriteLine(item.ToString());
-        }
-
-
-
-
         // TODO: Create a PrintItems() object overload that prints out ANY amount of objects passed in to the function as arguments
         public static void PrintItems(params object[] items)
         {
